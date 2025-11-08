298541b3da7a6daa860eca37071142eef8b5ae9b
manjunath25 <k.mnjunth@gmail.com>
2025-11-08 08:54:27 +0530

    ShopEazzy spring boot project created

---
+++ src/main/java/com/shopEZ/ShopEazzy/ShopEazzyApplication.java
@@ -0,0 +1,13 @@
+package com.shopEZ.ShopEazzy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopEazzyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopEazzyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shopEZ/ShopEazzy/ShopEazzyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopEZ.ShopEazzy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopEazzyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
